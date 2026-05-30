173f14e802327f60e34d151195db43686fc62cad
Jairo0624 <alvaradogonzalezjairo@gmail.com>
2026-05-29 22:17:37 -0600

    Medida arriesgada

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Login from "./Login";
 import Register from "./Register";
 import AdminDashboard from "./AdminDashboard";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
-//const API_URL = "http://localhost:3000";
+const API_URL = "https://api-troles.onrender.com" || "http://localhost:3000";
+//const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 const coloresSabores = {
   Limón: "#b7e85f",
--- src/Register.jsx
+++ src/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = "https://api-troles.onrender.com" || "http://localhost:3000";
 //const API_URL = "http://localhost:3000";
 
 function Register({ onRegister, onNavigateToLogin }) {
